b5f735d1e8b79e5372a538e11b58946dfab3e3d9
Aurélien Bompard <aurelien@bompard.org>
2022-07-12 15:54:28 +0200

    Update Sphinx
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/conf.py
+++ docs/conf.py
@@ -101,9 +101,9 @@ source_suffix = {
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 extlinks = {
-    "commit": ("https://github.com/fedora-infra/sqlalchemy-helpers/commit/%s", ""),
-    "issue": ("https://github.com/fedora-infra/sqlalchemy-helpers/issues/%s", "#"),
-    "pr": ("https://github.com/fedora-infra/sqlalchemy-helpers/pull/%s", "PR#"),
+    "commit": ("https://github.com/fedora-infra/sqlalchemy-helpers/commit/%s", "%s"),
+    "issue": ("https://github.com/fedora-infra/sqlalchemy-helpers/issues/%s", "#%s"),
+    "pr": ("https://github.com/fedora-infra/sqlalchemy-helpers/pull/%s", "PR#%s"),
 }
 
 # -- Misc -----
